f07db208637583a8844c5c35f42de09e33265799
Mounish Kokkula <k.mounish@gmail.com>
2017-10-18 09:16:39 -0400

    Add files via upload

--- Server.java
+++ Server.java
@@ -5,61 +5,76 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.*;
 
-public class Server {
+public class Server extends Thread {
 
 	public final static int SOCKET_PORT = 6664;
-	static String ServerFileLocation = "C:/Users/Mounish/Documents/books/ComputerNetworks/ServerMusic";
+	static int i = 1;
+	static Thread ti;
+	static String ServerFileLocation = "*Path*/ServerMusic";
 	static String sync = "";
 	static FileInputStream fis = null;
 	static BufferedInputStream bis = null;
-	// OutputStream os = null;
 	static ServerSocket servsock = null;
 	static Socket sock = null;
-	static DataOutputStream out = null; // new
-	// DataOutputStream(clientSocket.getOutputStream());
-	static DataInputStream in = null; // new
-	// DataInputStream(clientSocket.getInputStream());
+	static DataOutputStream out = null;
+	static DataInputStream in = null;
+//	public static Scanner se = null;
+	public static String fileName = "*Path*\Log.txt";
+	public static String line = null;
 
-	// FILE_TO_SEND +="a.mpeg";
-    public Server(Socket clientSocket) {
-        this.sock = clientSocket;
-    }
+	public Server(Socket clientSocket) {
+		this.sock = clientSocket;
+	}
 
-	
-		
-	
-//	public final static int SOCKET_PORT = 6664;
-	
-	public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
-				
-				//For every client starting a separate thread
+	// public final static int SOCKET_PORT = 6664;
+
+	public static void main(String[] args)
+			throws IOException, NoSuchAlgorithmException, InterruptedException, IllegalMonitorStateException {
+
+		// For every client starting a separate thread
 		servsock = new ServerSocket(SOCKET_PORT);
-		while (true) {
-			System.out.println("Waiting for request...");
+//		System.out.println("Enter 'Log' View log file\n or");
+//		System.out.println("Enter and key to connect to the client \n");
 
-	 
-			 
-			ClientWorker w;
-			 
-			try{
-			 
-			//server.accept returns a client connection
-			 
-			w = new ClientWorker(servsock.accept());
-			 
-			Thread t = new Thread(w);
-			 
-			t.start();
-			 
-			}catch(IOException e){
-			 
-			System.out.println("Accept failed: 4444");
-			 
-			System.exit(-1);
-			 
+		while (true) {
+
+			try {
+				// server.accept returns a client connection
+				
+				System.out.println("Waiting for request...");
+				System.out.println("Enter 'Log' View log file\n or");
+				System.out.println("Enter any key to connect to the client \n");
+				Scanner se = new Scanner(System.in);				
+				String input = se.nextLine();
+				if (!input.equalsIgnoreCase("Log")) {
+					//se.close();
+					ti = new Thread(new ClientWorker(servsock.accept()));
+					ti.start();
+					synchronized (ti) {
+						ti.wait();
+					}
+					i++;
+				} else {
+				FileReader fileReader = new FileReader(fileName);
+				
+					BufferedReader bufferedReader = new BufferedReader(fileReader);
+					while ((line = bufferedReader.readLine()) != null) {
+						System.out.println(line);
+					}
+					fileReader.close();
+					bufferedReader.close();
+				//	se.close();
+				}
 			}
+			
+				catch (IOException e) {
+
+				System.out.println("Accept failed: 4444");
+
+				System.exit(-1);
+			 }
 			}
 		
-	}	}
 	
-
+		}
+}
